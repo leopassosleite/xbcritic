01223044289645b01c78f2f64ebd5b9f437d0979
leopassosleite <leopassos.leite4@gmail.com>
2022-04-21 20:05:06 -0300

    Project create, Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css'
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="xbcritic-nav-content">
+                    <h1>XBCritic</h1>
+                    <a href="https://github.com/leopassosleite">
+                        <div className="xbcritic-contact-container">
+                            <p className="xbcritic-contact-link">/leopassosleite</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
