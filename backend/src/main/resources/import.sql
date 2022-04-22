6c93fbf4d922ba31a61590bb59ed0e4bf2964751
leopassosleite <leopassos.leite4@gmail.com>
2022-04-22 04:33:10 -0300

    Domain model, database seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_movie(score, count, title, image, video) VALUES (0, 0, 'The Witcher III - Wild Hunt', 'https://store-images.s-microsoft.com/image/apps.28990.69531514236615003.8f0d03d6-6311-4c21-a151-834503c2901a.d629260e-2bc4-4588-950c-f278cbc22a64?q=90&w=177&h=265', 'https://www.youtube.com/watch?v=ehjJ614QfeM');
+
+INSERT INTO tb_user(name) VALUES ('Leonardo');
+
+INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 1 , 3.5);
